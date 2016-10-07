acd8899f186fd78c95cade9ed9cca93825afba09
Jamie Hofer <jamie.k.hofer@gmail.com>
2016-10-07 16:09:35 -0400

    Add Compatibility for Mecanum Wheels

--- Main.c
+++ Main.c
@@ -57,13 +57,28 @@ task autonomous(){
 }
 
 task usercontrol(){
+int X2 = 0, Y1 = 0, X1 = 0, threshold = 15;
 	while(true){
-		//all *-1 are because of the orientation of the motors
-		motor[leftFront] = vexRT[Ch3]*-1;
-		motor[leftBack] = vexRT[Ch3];
-		motor[rightFront] = vexRT[Ch2]*-1;
-		motor[rightBack] = vexRT [Ch2]*-1;
-	    	SensorValue[Pneumatic1] = vexRT[Btn7U];
-		SensorValue[Pneumatic2] = vexRT[Btn7U];
-		}
+//Create deadzone for Y1/Ch3
+if(abs(vexRT[Ch3]) > threshold)
+Y1 = vexRT[Ch3];
+else
+Y1 = 0;
+//Create deadzone for X1/Ch4
+if(abs(vexRT[Ch4]) > threshold)
+X1 = vexRT[Ch4];
+else
+X1 = 0;
+//Create deadzone for X2/Ch1
+if(abs(vexRT[Ch1]) > threshold)
+X2 = vexRT[Ch1];
+else
+X2 = 0;
+ 
+//Remote Control Commands
+motor[frontRight] = Y1 - X2 - X1;
+motor[backRight] =  Y1 - X2 + X1;
+motor[frontLeft] = Y1 + X2 + X1;
+motor[backLeft] =  Y1 + X2 - X1;
+}
 }
